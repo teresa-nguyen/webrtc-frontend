c41ef6b50ea9b40e81530c7ab8752f3031a2d7df
teresa-nguyen <nguyen.tsa@gmail.com>
2022-04-28 01:17:39 -0700

    add material ui and update intro page

--- src/IntroductionPage/IntroductionPage.js
+++ src/IntroductionPage/IntroductionPage.js
@@ -1,21 +1,56 @@
-import React, { useEffect } from "react";
-import logo from "../resources/images/logo.png";
-import ConnectingButtons from "./ConnectingButtons";
-import { connect } from "react-redux";
-import { setIsRoomHost } from "../store/actions";
-import "./IntroductionPage.css";
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { setIsRoomHost } from '../store/actions';
+import './IntroductionPage.css';
+import {
+  Container,
+  Stack,
+  Button,
+  Paper,
+  Typography,
+  Grid,
+} from '@mui/material';
+import { useHistory } from 'react-router-dom';
 
 const IntroductionPage = ({ setIsRoomHostAction }) => {
   useEffect(() => {
     setIsRoomHostAction(false);
   }, []);
 
+  let history = useHistory();
+
+  const pushToJoinRoomPage = () => {
+    history.push('/join-room');
+  };
+
+  const pushToJoinRoomPageAsHost = () => {
+    history.push('/join-room?host=true');
+  };
+
   return (
-    <div className="introduction_page_container">
-      <div className="introduction_page_panel">
-        <img src={logo} className="introduction_page_image"></img>
-        <ConnectingButtons />
-      </div>
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+      }}
+    >
+      <Container maxWidth='sm'>
+        <Paper elevation={12} style={{ padding: '24px 120px' }}>
+          <Typography variant='h3' component='h1' textAlign='center' pb={6}>
+            Meeting
+          </Typography>
+          <Stack spacing={2}>
+            <Button variant='contained' onClick={pushToJoinRoomPage}>
+              Join a meeting
+            </Button>
+            <Button variant='outlined' onClick={pushToJoinRoomPageAsHost}>
+              Host a meeting
+            </Button>
+          </Stack>
+        </Paper>
+      </Container>
     </div>
   );
 };
