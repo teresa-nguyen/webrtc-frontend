c1c3abe8890ea9d59e4189f4137e176314e0d242
teresa-nguyen <nguyen.tsa@gmail.com>
2022-05-04 01:08:05 -0700

    clean up join room page

--- src/JoinRoomPage/JoinRoomPage.js
+++ src/JoinRoomPage/JoinRoomPage.js
@@ -1,14 +1,67 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { setIsRoomHost } from '../store/actions';
-import JoinRoomContent from './JoinRoomContent';
-import './JoinRoomPage.css';
+import { setIsRoomHost, setIdentity, setRoomId } from '../store/actions';
+import { useHistory } from 'react-router-dom';
+import { getRoomExists } from '../utils/api';
+import { Container, Paper, Typography, TextField, Button } from '@mui/material';
 
 const JoinRoomPage = (props) => {
-  const { setIsRoomHostAction, isRoomHost } = props;
+  const {
+    setIsRoomHostAction,
+    isRoomHost,
+    setIdentityAction,
+    setRoomIdAction,
+  } = props;
   const search = useLocation().search;
   const titleText = isRoomHost ? 'Host meeting' : 'Join meeting';
+  // first input
+  const [roomIdValue, setRoomIdValue] = useState('');
+  const handleRoomIdValueChange = (event) => {
+    setRoomIdValue(event.target.value);
+  };
+  // second input
+  const [nameValue, setNameValue] = useState('');
+  const handleNameValueChange = (event) => {
+    setNameValue(event.target.value);
+  };
+  //error message
+  const [errorMessage, setErrorMessage] = useState(null);
+  //buttons
+  const successButtonText = isRoomHost ? 'Host' : 'Join';
+  const history = useHistory();
+  const createRoom = () => {
+    history.push('/room');
+  };
+  const joinRoom = async () => {
+    const responseMessage = await getRoomExists(roomIdValue);
+
+    const { roomExists, full } = responseMessage;
+    if (roomExists) {
+      if (full) {
+        setErrorMessage('Meeting is full. Please try again later.');
+      } else {
+        //join a room !
+        //save in our redux store meeting id which was provided by user which would like to join
+        setRoomIdAction(roomIdValue);
+        history.push('/room');
+      }
+    } else {
+      setErrorMessage('Meeting not found. Check your meeting id.');
+    }
+  };
+  const handleJoinRoom = async () => {
+    setIdentityAction(nameValue);
+    if (isRoomHost) {
+      createRoom();
+    } else {
+      await joinRoom();
+    }
+  };
+
+  const pushToIntroductionPage = () => {
+    history.push('/');
+  };
 
   useEffect(() => {
     const isRoomHost = new URLSearchParams(search).get('host');
@@ -18,12 +71,44 @@ const JoinRoomPage = (props) => {
   }, []);
 
   return (
-    <div className='join_room_page_container'>
-      <div className='join_room_page_panel'>
-        <p className='join_room_title'>{titleText}</p>
-        <JoinRoomContent />
-      </div>
-    </div>
+    <Container maxWidth='sm'>
+      <Paper elevation={12} style={{ padding: '24px 120px' }}>
+        <Typography variant='h4' component='h1'>
+          {titleText}
+        </Typography>
+        <form>
+          {!isRoomHost && (
+            <TextField
+              margin='normal'
+              label='Enter meeting ID'
+              variant='outlined'
+              value={roomIdValue}
+              onChange={handleRoomIdValueChange}
+            />
+          )}
+          <TextField
+            margin='normal'
+            label='Enter your Name'
+            variant='outlined'
+            value={nameValue}
+            onChange={handleNameValueChange}
+          />
+          {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+          <div style={{ display: 'flex', justifyContent: 'flex-end' }}>
+            <Button
+              style={{ marginRight: 12 }}
+              variant='contained'
+              onClick={handleJoinRoom}
+            >
+              {successButtonText}
+            </Button>
+            <Button variant='outlined' onClick={pushToIntroductionPage}>
+              Cancel
+            </Button>
+          </div>
+        </form>
+      </Paper>
+    </Container>
   );
 };
 
@@ -36,6 +121,8 @@ const mapStoreStateToProps = (state) => {
 const mapActionsToProps = (dispatch) => {
   return {
     setIsRoomHostAction: (isRoomHost) => dispatch(setIsRoomHost(isRoomHost)),
+    setIdentityAction: (identity) => dispatch(setIdentity(identity)),
+    setRoomIdAction: (roomId) => dispatch(setRoomId(roomId)),
   };
 };
 
