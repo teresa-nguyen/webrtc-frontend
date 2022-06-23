57faa40192f9dba65511296d30d6bd132b3306c5
teresa-nguyen <nguyen.tsa@gmail.com>
2022-06-22 22:07:18 -0700

    fix server route

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
-import axios from "axios";
+import axios from 'axios';
 
-const serverApi = "http://localhost:5002/api";
+const serverApi = '/api';
 
 export const getRoomExists = async (roomId) => {
   const response = await axios.get(`${serverApi}/room-exists/${roomId}`);
